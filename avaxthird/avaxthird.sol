d29ce15e16ccea4505fe137f2af2066a97394c5b
amateurre <31176886+amateurre@users.noreply.github.com>
2023-11-20 22:31:25 +0800

    Update avaxthird.sol

--- avaxthird/avaxthird.sol
+++ avaxthird/avaxthird.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts@4.9/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9/access/Ownable.sol";
+
+contract mod3 is ERC20, Ownable {
+    constructor() ERC20("Avalanche", "AVAX") {
+        _mint(msg.sender, 20);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+}
